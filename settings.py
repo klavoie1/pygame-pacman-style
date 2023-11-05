454b2826e9a4b8764f33794787bdd30a61b84142
Kenneth <129323640+klavoie1@users.noreply.github.com>
2023-11-05 18:58:02 -0500

    Removed player and enemy values

--- settings.py
+++ settings.py
@@ -4,11 +4,3 @@ class Settings:
         self.screen_width = 1200
         self.screen_height = 1000
         self.bg_color = (0, 0, 0)  # Background color
-
-        # Player settings
-        self.player_speed = 5
-
-        # Enemy settings
-        self.enemy_speed = 3
-
-
