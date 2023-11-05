1787ab3f02146f6563253f810045eb4dd4185fa7
Kenneth <129323640+klavoie1@users.noreply.github.com>
2023-11-05 17:59:09 -0500

    Fixed glitch with movement speed

--- player.py
+++ player.py
@@ -17,20 +17,26 @@ class Player:
         self.moving_left = False
         self.moving_right = False
 
+        self.x_pos = self.rect.centerx  # Use a separate float attribute for x position
+        self.y_pos = self.rect.centery  # Use a separate float attribute for y position
+        self.speed = 0.2  # Using a small float value for slower movement
 
     def update(self):
         # Update player position based on movement flags
 
         # Values below 1 make it so the keydowns don't work: Need to Fix!!
-        
+
         if self.moving_up and self.rect.top > 0:
-            self.rect.y -= 1
+            self.y_pos -= self.speed
         if self.moving_down and self.rect.bottom < self.screen.get_height():
-            self.rect.y += 1
+            self.y_pos += self.speed
         if self.moving_left and self.rect.left > 0:
-            self.rect.x -= 1
+            self.x_pos -= self.speed
         if self.moving_right and self.rect.right < self.screen.get_width():
-            self.rect.x += 1
+            self.x_pos += self.speed
+
+        self.rect.centerx = int(self.x_pos)
+        self.rect.centery = int(self.y_pos)
 
     def draw_player(self):
         # Draw the player on the screen
