8850d2a841db0f80b8f538695ff7575da5873fbe
Kenneth <129323640+klavoie1@users.noreply.github.com>
2023-11-05 17:59:42 -0500

    Changed screen size to a larger one

--- settings.py
+++ settings.py
@@ -1,8 +1,8 @@
 class Settings:
     def __init__(self):
         # Screen settings
-        self.screen_width = 800
-        self.screen_height = 600
+        self.screen_width = 1200
+        self.screen_height = 1000
         self.bg_color = (0, 0, 0)  # Background color
 
         # Player settings
